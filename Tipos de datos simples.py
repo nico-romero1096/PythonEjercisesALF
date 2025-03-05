1c2b0c81d3a4e4f9ba4a8a537e3bbc4918deba87
Nicolas Romero <102483311+nico-romero1096@users.noreply.github.com>
2025-03-05 00:38:35 -0300

    Actualizacion ejercicios

---
+++ Tipos de datos simples.py
@@ -0,0 +1,42 @@
+#Descomentar linea de codigo para ejecutar
+
+
+#Ejercicios 1
+
+# print("Hola Mundo!")
+
+
+#Ejercicio 2
+
+# mensaje = "Hola Mundo!"
+
+# print(mensaje)
+
+
+#Ejercicio 3
+
+# nombre_usuario = input("Introduzca nombre de usuario: ")
+
+# print(f"¡Hola {nombre_usuario}!")
+
+
+#Ejercicio 4
+
+# print(((3+2)/(2*5))**2)
+
+
+#Ejercicio 5
+
+# horas = float(input("Horas trabajadas: "))
+# coste = float(input("Coste por horas trabajadas: "))
+
+# paga = horas*coste
+
+# print(f"La paga es: {paga}")
+
+
+#Ejercicio 6
+
+
+
+
