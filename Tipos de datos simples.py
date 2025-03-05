861bd984b3eb7557bc1e87ff33f59de13856d3dd
Nicolas Romero <102483311+nico-romero1096@users.noreply.github.com>
2025-03-05 03:21:51 -0300

    Se hizo ejercicios del 1 al 11

--- Tipos de datos simples.py
+++ Tipos de datos simples.py
@@ -1,7 +1,7 @@
 #Descomentar linea de codigo para ejecutar
 
 
-#Ejercicios 1
+#Ejercicio 1
 
 # print("Hola Mundo!")
 
@@ -37,6 +37,66 @@
 
 #Ejercicio 6
 
+# n = int(input("Introduce un numero entero positivo: "))
+
+# suma = (n*(n+1))//2
+
+# print(f"La suma de numeros enteros desde el 1 hasta {n} es: {suma} ")
+
+
+#Ejercicio 7
+
+# peso = float(input("Indique su peso en kg: "))
+# estatura = float(input("Indique su estatura en metros: "))
+
+# imc = peso / (estatura**2)
+
+# print(f"Tu índice de masa corporal es {round(imc, 2)}")
+
+
+#Ejercicio 8
+
+# n = int(input("Ingrese un numero entero: "))
+# m = int(input("Ingrese otro numero entero: "))
+
+# c = n // m
+
+# r = n % m
+
+# print(f"{n} entre {m} da un cociente de {c} y un resto de {r}")
+
+#Ejercicio 9
+
+# cantidad = float(input("Ingrese la cantidad de capital que desea invertir: "))
+# interes = float(input("Indique el interes anual: "))
+# años = int(input("Numero de años que desea mantener la inversion: "))
+
+# print("Capital final: " + str(round(cantidad * (interes / 100 + 1) ** años, 2)))
+
+
+#Ejercicio 10
+
+# payasos = int(input("Ingrese el numero de payasos a enviar: "))
+# muñecas = int(input("Ingrese el numero de muñecas a enviar: "))
+# peso_payaso = 112
+# peso_muñeca = 75
+# peso_total = payasos*peso_payaso + muñecas*peso_muñeca
+
+# print(f"El peso total del paquete es de {peso_total} g")
+
+
+#Ejercicio 11
+
+# inversion = float(input("Introduce la inversión inicial: "))
+# interes = 0.04
+# balance1 = inversion * (1 + interes)
+# print("Balance tras el primer año:" + str(round(balance1, 2)))
+# balance2 = balance1 * (1 + interes)
+# print("Balance tras el segundo año:" + str(round(balance2, 2)))
+# balance3 = balance2 * (1 + interes)
+# print("Balance tras el tercer año:" + str(round(balance3, 2)))
+
+#Ejercicio 12
 
 
 
