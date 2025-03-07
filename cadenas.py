b6540e23af31d4dfad387639aeb7e16e0f0db2cd
Nicolas <nicolasromero1096@gmail.com>
2025-03-07 15:43:04 -0300

    cadenas

--- cadenas.py
+++ cadenas.py
@@ -1,7 +1,13 @@
 #Ejercicio 1
 
-usuario = input("Introduzca nombre su usuario: ")
-numero = int(input("Indique un numero entero: "))
+# usuario = input("Introduzca nombre su usuario: ")
+# numero = int(input("Indique un numero entero: "))
 
-print((usuario+"\n")*numero)
+# print((usuario+"\n")*numero)
 
+#Ejercicio 2
+
+nombre = input("Ingrese su nombre: ")
+apellido = input("Ingrese su apellido: ")
+
+print(nombre.capitalize() + " " +)
