866ecadf105253f9e575337e2e7fea74fa1e48ca
Nicolas <nicolasromero1096@gmail.com>
2025-03-07 16:54:58 -0300

    Se agrega la seccion condicionales

---
+++ condicionales.py
@@ -0,0 +1,70 @@
+#Ejercicio 1:
+'''
+edad = int(input("Cual es su edad? "))
+
+if edad >= 18:
+    print("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")
+'''
+
+#Ejercicio 2:
+'''
+key = "contraseña"
+password = input("Ingrese su contraseña: ")
+
+if key == password.lower():
+    print("Contraseña correcta.")
+else:
+    print("Contraseña incorrecta.")
+'''
+
+#Ejercicio 3:
+'''
+num1 = int(input("Ingrese un numero: "))
+num2 = int(input("Ingrese otro numero: "))
+
+if num2 == 0:
+    print("Error! No se puede dividir por cero.")
+else:
+    print(f"El resultado de la division de ambos numeros es: {num1/num2}")
+'''
+
+#Ejercicio 4:
+'''
+numero = int(input("Ingrese un numero entero: "))
+
+if numero%2 == 0:
+    print(f"El numero {numero} es par")
+else:
+    print(f"El numero {numero} es impar")
+'''
+
+#Ejercicio 5
+'''
+edad = int(input("Ingrese su edad: "))
+ingresos = float(input("Indique sus ingresos mensuales: "))
+
+if edad > 16 and ingresos >= 1000:
+    print("Tiene que tributa")
+else:
+    print("No tiene que tributar")
+'''
+
+#Ejercicio 6:
+'''
+name = input("¿Cómo te llamas? ")
+gender = input("¿Cuál es tu sexo (M o H)? ")
+if (gender == "M" and name.lower() < 'm') or (gender == "H" and name.lower() > 'n'):
+    group = "A"
+else:
+    group = "B"
+print("Tu grupo es " + group)
+'''
+
+#Ejercicio 7:
+
+
+
+
+
